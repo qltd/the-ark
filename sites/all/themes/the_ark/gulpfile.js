e3aa6e3966e81116f37a040f41006fb63ab24995
mjbondra <mjbondra@gmail.com>
2014-07-10 13:54:04 -0400

    update gulpfile; update package.json; ignore comiled js

--- sites/all/themes/the_ark/gulpfile.js
+++ sites/all/themes/the_ark/gulpfile.js
@@ -3,40 +3,38 @@ var gulp = require('gulp')
   , browserify = require('gulp-browserify')
   , compass = require('gulp-compass')
   , csso = require('gulp-csso')
-  , footer = require('gulp-footer')
   , plumber = require('gulp-plumber')
   , rename = require('gulp-rename')
   , uglify = require('gulp-uglify');
 
 gulp.task('bower', function () {
   return bower()
-    .pipe(gulp.dest('./client/assets/lib/'));
+    .pipe(gulp.dest('./libraries/'));
 });
 
 gulp.task('browserify', ['bower'], function () {
-  gulp.src('./client/config/app.js')
+  gulp.src('./js/*.js')
     .pipe(plumber())
-    .pipe(browserify())
-    .pipe(gulp.dest('./client/assets/js/'))
-    .pipe(rename('app.min.js'))
-    .pipe(uglify({ outSourceMap: true }))
-    .pipe(footer('\n//# sourceMappingURL=app.min.js.map'))
-    .pipe(gulp.dest('./client/assets/js/'));
+    // .pipe(browserify())
+    // .pipe(gulp.dest('./js/'))
+    .pipe(rename(function (path) { path.basename += '.min'; }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./js/'));
 });
 
 gulp.task('compass', ['bower'], function () {
-  gulp.src('./client/assets/scss/*.scss')
+  gulp.src('./sass/*.scss')
     .pipe(plumber())
-    .pipe(compass({ sass: './client/assets/scss', css: './client/assets/css' }))
-    .pipe(gulp.dest('./client/assets/css/'))
+    .pipe(compass({ environment: 'production', sass: './sass', css: './css' }))
+    .pipe(gulp.dest('./css/'))
     .pipe(rename(function (path) { path.basename += '.min'; }))
     .pipe(csso())
-    .pipe(gulp.dest('./client/assets/css/'));
+    .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('watch', function () {
-  gulp.watch(['./client/config/*.js', './client/app/*.js'], ['browserify']);
-  gulp.watch(['./client/assets/scss/*.scss', './client/assets/scss/**/*.scss'], ['compass']);
+  gulp.watch(['./js/*.js'], ['browserify']);
+  gulp.watch(['./sass/*.scss', './sass/**/*.scss'], ['compass']);
 });
 
 gulp.task('default', ['bower', 'browserify', 'compass']);
