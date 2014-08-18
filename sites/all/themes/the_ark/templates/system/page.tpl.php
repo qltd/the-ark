308c03fb54b9820760baca86c205f6bdccc10b54
mjbondra <mjbondra@gmail.com>
2014-08-17 21:11:25 -0400

    update page template and theme info

--- sites/all/themes/the_ark/templates/system/page.tpl.php
+++ sites/all/themes/the_ark/templates/system/page.tpl.php
@@ -75,6 +75,9 @@
       <?php print $breadcrumb; ?>
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
+      <?php if ($title): ?>
+        <h1 class="content-title"><?php print $title; ?></h1>
+      <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
       <?php print render($tabs); ?>
@@ -82,7 +85,7 @@
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
       <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
+      <?php // print $feed_icons; ?>
     </div>
 
     <?php print render($page['sidebar']); ?>
