c9af473372e015a49750b74d5a1a9cb2f19a4491
mjbondra <mjbondra@gmail.com>
2014-08-30 21:38:55 -0400

    update admin subtheme

---
+++ sites/all/themes/the_ark_admin/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp')
+  , browserify = require('gulp-browserify')
+  , compass = require('gulp-compass')
+  , csso = require('gulp-csso')
+  , plumber = require('gulp-plumber')
+  , rename = require('gulp-rename')
+  , uglify = require('gulp-uglify');
+
+
+gulp.task('browserify', function () {
+  gulp.src('./js-src/the-ark-admin.js')
+    .pipe(plumber())
+    .pipe(browserify())
+    .pipe(gulp.dest('./js/'))
+    .pipe(rename(function (path) { path.basename += '.min'; }))
+    .pipe(uglify({ outSourceMap: true }))
+    .pipe(gulp.dest('./js/'));
+});
+
+gulp.task('compass', function () {
+  gulp.src('./sass/*.scss')
+    .pipe(plumber())
+    .pipe(compass({ environment: 'production' }))
+    .pipe(gulp.dest('./css/'))
+    .pipe(rename(function (path) { path.basename += '.min'; }))
+    .pipe(csso())
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./js-src/*.js', './js-src/**/*.js'], ['browserify']);
+  gulp.watch(['./sass/*.scss', './sass/**/*.scss'], ['compass']);
+});
+
+gulp.task('default', ['browserify', 'compass']);
