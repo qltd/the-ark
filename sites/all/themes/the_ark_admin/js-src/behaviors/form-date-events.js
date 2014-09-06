0de0fbc45432accb6f2e953d71724494938c744f
mjbondra <mjbondra@gmail.com>
2014-09-06 09:30:55 -0400

    update admin js; update .jshitrc

--- sites/all/themes/the_ark_admin/js-src/behaviors/form-date-events.js
+++ sites/all/themes/the_ark_admin/js-src/behaviors/form-date-events.js
@@ -1,5 +1,20 @@
 (function ($) {
-  Drupal.admin.behaviors.theArkAdminFormDateEvents = {
-    attach: function () {}
+  Drupal.admin.behaviors.theArkAdminFormDateEvents = function () {
+    var eventForm = $('#event-node-form');
+    if (eventForm.length === 0) return;
+
+    var dateDoorsField = $('.form-item-field-date-doors-und-0-value-date .form-text')
+      , dateShowField = $('.form-item-field-date-und-0-value-date .form-text');
+
+    if (dateDoorsField.length === 0 || dateShowField.length === 0) return;
+
+    var dateShowFieldValue = dateShowField.val(); // store value for comparative purposes
+
+    dateShowField.change(function () {
+      // only change doors date if it was previously the same as the show date
+      if (dateDoorsField.val() === dateShowFieldValue) dateDoorsField.val(dateShowField.val());
+      dateShowFieldValue = dateShowField.val();
+    });
+
   };
 })(jQuery);
