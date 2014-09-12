335760de3a6603d3b0a10f0aee6c4ae02b413bd3
mjbondra <mjbondra@gmail.com>
2014-09-12 12:06:56 -0400

    update view template for edit sound tech user link field

--- sites/all/themes/the_ark_admin/templates/views/views-view-field--sound-tech-users-block--edit-node.tpl.php
+++ sites/all/themes/the_ark_admin/templates/views/views-view-field--sound-tech-users-block--edit-node.tpl.php
@@ -11,13 +11,15 @@ $user_roles = isset($row->_field_data['uid']['entity']->roles)
   : array();
 $user_id = isset($row->_field_data['uid']['entity']->uid)
   ? $row->_field_data['uid']['entity']->uid
-  : array();
+  : '';
 $edit_link = isset($field->last_render)
   ? $field->last_render
   : '';
 
 if (isset($user_roles[5]) && count($user_roles) === 2) {
-  print l(t('edit'), 'admin/sound-tech/people/'. $user_id .'/edit');
+  if (isset($user->roles[4]) || isset($user->roles[3]) || $user->uid === $user_id) {
+    print l(t('edit'), 'admin/sound-tech/people/'. $user_id .'/edit');
+  }
 } elseif ($user->uid === $user_id) {
   print $edit_link;
 }
