3712a19717e4d828bcec5c437205b646b05ac5ad
mjbondra <mjbondra@gmail.com>
2014-09-23 09:18:28 -0400

    update header styles

--- sites/all/themes/the_ark/template.php
+++ sites/all/themes/the_ark/template.php
@@ -5,3 +5,12 @@
  * Template overrides as well as (pre-)process and alter hooks for the
  * the-ark theme.
  */
+function the_ark_form_alter(&$form, &$form_state, $form_id) {
+  if ($form_id == 'search_block_form') {
+    $form['actions']['submit'] = array( // replace search submit input tag with button tag
+      '#prefix' => '<button type="submit" class="form-submit">',
+      '#suffix' => '</button>',
+      '#markup' => t('Search'),
+    );
+  }
+}
