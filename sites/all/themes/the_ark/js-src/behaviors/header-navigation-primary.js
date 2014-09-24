147f02391aa38ab692e169e206f311bdce518503
mjbondra <mjbondra@gmail.com>
2014-09-24 19:04:30 -0400

    update navigation styles

--- sites/all/themes/the_ark/js-src/behaviors/header-navigation-primary.js
+++ sites/all/themes/the_ark/js-src/behaviors/header-navigation-primary.js
@@ -2,13 +2,21 @@
   Drupal.behaviors.theArkHeaderNavigationPrimary = {
     attach: function () {
       var navigation = $('.menu-primary-wrapper')
-        , toggle = $('.header-navigation-primary .block__title');
-      if (navigation.length === 0 || toggle.length === 0) return;
+        , toggle = $('.header-navigation-primary .block__title')
+        , buttonClose = $('.menu-primary-close');
+      if (navigation.length === 0 || toggle.length === 0 || buttonClose.length === 0) return;
 
-      toggle.click(function () {
+      var toggleClasses = function () {
         $('body').toggleClass('document-no-scroll');
         toggle.toggleClass('active');
         navigation.toggleClass('menu-flyout-visible');
+      };
+
+      toggle.click(function () {
+        toggleClasses();
+      });
+      buttonClose.click(function () {
+        toggleClasses();
       });
     }
   };
