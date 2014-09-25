e3304b2850423d47ff8fbc0806c1b5ee835ba328
mjbondra <mjbondra@gmail.com>
2014-09-24 22:49:06 -0400

    squish mobile bugs

--- sites/all/themes/the_ark/js-src/behaviors/header-navigation-primary.js
+++ sites/all/themes/the_ark/js-src/behaviors/header-navigation-primary.js
@@ -2,15 +2,16 @@
   Drupal.behaviors.theArkHeaderNavigationPrimary = {
     attach: function () {
       var body = $('body')
-        , navigation = $('.menu-primary-wrapper')
+        , wrapper = $('.menu-primary-wrapper')
+        , navigation = $('.menu-primary-wrapper > .menu')
         , toggle = $('.header-navigation-primary .block__title')
         , buttonClose = $('.menu-primary-close');
-      if (navigation.length === 0 || toggle.length === 0 || buttonClose.length === 0) return;
+      if (wrapper.length === 0 || navigation.length === 0) return;
 
       var toggleClasses = function (method) {
         body[method]('document-no-scroll');
         toggle[method]('active');
-        navigation[method]('menu-flyout-visible');
+        wrapper[method]('menu-flyout-visible');
         switch (method) {
           case 'addClass':
             $(document).bind('touchmove', function (e) {
@@ -27,16 +28,20 @@
         e.stopPropagation();
       });
 
-      body.bind('swiperight', function () {
+      body.bind('swiperight', function (e) {
+        e.preventDefault();
         toggleClasses('addClass');
       });
-      navigation.bind('swipeleft', function () {
+      navigation.bind('swipeleft', function (e) {
+        e.preventDefault();
         toggleClasses('removeClass');
       });
-      toggle.bind('tap', function () {
+      toggle.bind('tap', function (e) {
+        e.preventDefault();
         toggleClasses('addClass');
       });
-      buttonClose.bind('tap', function () {
+      buttonClose.bind('tap', function (e) {
+        e.preventDefault();
         toggleClasses('removeClass');
       });
     }
