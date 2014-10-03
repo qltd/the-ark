f45f72d904e3e80a6b116c89c6dbbf29b39005d7
mjbondra <mjbondra@gmail.com>
2014-10-03 15:44:00 -0400

    update header navigation

--- sites/all/themes/the_ark/js-src/behaviors/region-header.js
+++ sites/all/themes/the_ark/js-src/behaviors/region-header.js
@@ -6,7 +6,7 @@
 
       Headroom.options = Headroom.options || {};
       if ($('.front').length === 0) Headroom.options.offset = 40;
-      else Headroom.options.offset = 340;
+      else Headroom.options.offset = 220;
       header.headroom();
     }
   };
