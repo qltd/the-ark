228e03f74d09c882860f9ea836b7ad7088f64731
mjbondra <mjbondra@gmail.com>
2014-12-17 15:02:42 -0500

    add template and global styles for tabs

---
+++ sites/all/themes/the_ark/templates/views/views-view-field--field-tabs.tpl.php
@@ -0,0 +1,18 @@
+<?php
+
+if (isset($row->field_field_tabs[0]['rendered']['entity']['field_collection_item'])):
+  foreach ($row->field_field_tabs[0]['rendered']['entity']['field_collection_item'] as $key => $item) {
+    $title = isset($item['field_tab_title']['#items'][0]['safe_value']) ?
+      $item['field_tab_title']['#items'][0]['safe_value'] :
+      'Tab' . ($key + 1);
+    $body = isset($item['field_tab_body']['#items'][0]['safe_value']) ?
+      $item['field_tab_body']['#items'][0]['safe_value'] :
+      '';
+  }
+?>
+
+<h2 class="tabs-title expander"><?php print $title; ?></h2>
+<div class="tabs-content expander-content"><?php print $body; ?></div>
+
+<?php
+endif;
