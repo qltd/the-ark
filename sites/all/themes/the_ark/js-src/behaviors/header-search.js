5b6fa93b73403bb7269c894c51b5a09738b0bec2
mjbondra <mjbondra@gmail.com>
2015-01-12 18:24:48 -0500

    add mobile search styles

--- sites/all/themes/the_ark/js-src/behaviors/header-search.js
+++ sites/all/themes/the_ark/js-src/behaviors/header-search.js
@@ -2,14 +2,38 @@
   Drupal.behaviors.theArkHeaderSearch = {
     attach: function () {
       var block = $('.header-search')
+        , doc = $(document)
         , form = $('.header-search .search-block-form')
         , submit = $('.header-search .form-submit')
-        , text = $('.header-search .form-text');
+        , text = $('.header-search .form-text')
+        , textContainer = $('.header-search .form-type-textfield');
 
       if (block.length === 0) return;
 
-      submit.click(function (event) {
+      submit.on('tap', function (event) {
         event.preventDefault();
+        event.stopPropagation();
+
+        if (textContainer.hasClass('form-type-textfield-active')) {
+          textContainer.removeClass('form-type-textfield-active');
+        } else {
+          textContainer.addClass('form-type-textfield-active');
+          text.focus();
+
+          doc.on('tap', function (event) {
+            textContainer.removeClass('form-type-textfield-active');
+            doc.off('tap');
+          });
+        }
+      });
+
+      submit.on('click', function (event) {
+        event.preventDefault();
+        if (text.val()) form.submit();
+      });
+
+      text.on('tap', function (event) {
+        event.stopPropagation();
       });
     }
   };
