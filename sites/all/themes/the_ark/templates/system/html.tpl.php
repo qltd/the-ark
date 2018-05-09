2f3efe6b0fd6a3c87977fd271dafc535876d86ef
Matt Restorff <inperpetuum@gmail.com>
2018-05-09 12:29:29 -0400

    Site updates

--- sites/all/themes/the_ark/templates/system/html.tpl.php
+++ sites/all/themes/the_ark/templates/system/html.tpl.php
@@ -58,15 +58,8 @@
   <?php print $head; ?>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-</head>
-<body<?php print $attributes;?>>
-  <div id="fb-root"></div>
-  <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-  <?php print $page_top; ?>
-  <?php print $page; ?>
-  <?php print $page_bottom; ?>
 
-    <script>
+  <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
       m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -76,5 +69,14 @@
       ga('send', 'pageview');
 
     </script>
+</head>
+<body<?php print $attributes;?>>
+  <div id="fb-root"></div>
+  <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+  <?php print $page_top; ?>
+  <?php print $page; ?>
+  <?php print $page_bottom; ?>
+
+
 </body>
 </html>
